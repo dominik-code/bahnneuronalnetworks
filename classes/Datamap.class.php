a2ec7e3c9a5be0c6ba600a385b69134f726cb6ff
dominik-code <git@dominik-schmitt.eu>
2019-07-06 18:14:31 +0200

    Added db

--- classes/Datamap.class.php
+++ classes/Datamap.class.php
@@ -1,5 +1,8 @@
 <?php
 
+require_once './MysqliDb.class.php';
+require_once '../config/statusconfig.php';
+
 class Datamap {
 
     protected $type = "";
@@ -13,8 +16,8 @@ class Datamap {
                 $fromvalue = $key;
                 $tovalue = $value;
             }
-//            $mysqli = new mysqli(SETTING_DB_IP, SETTING_DB_USER, SETTING_DB_PASSWORD, SETTING_DB_NAME);
-//            $mysqli->query("INSERT INTO `datamap` (`id`, `datatype`, `fromvalue`, `tovalue`) VALUES (NULL, '$type', '$fromvalue', '$tovalue')");
+            $mysqli = new mysqli(SETTING_DB_IP, SETTING_DB_USER, SETTING_DB_PASSWORD, SETTING_DB_NAME);
+            $mysqli->query("INSERT INTO `datamap` (`id`, `datatype`, `fromvalue`, `tovalue`) VALUES (NULL, '$type', '$fromvalue', '$tovalue')");
         }
     }
 
